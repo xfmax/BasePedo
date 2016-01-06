308fb6d72a1f684704cdf041feb40cd5f7e74e53
xf <xufengbase@gmail.com>
2016-01-06 10:17:44 +0800

    修整一些参数的说明，优化代码结构

--- app/src/main/java/com/base/basepedo/ui/MainActivity.java
+++ app/src/main/java/com/base/basepedo/ui/MainActivity.java
@@ -3,33 +3,24 @@ package com.base.basepedo.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.base.basepedo.R;
 import com.base.basepedo.service.StepDcretor;
 import com.base.basepedo.service.StepService;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity implements Handler.Callback{
 	private TextView text_step;
 	private final int STEPSHOW = 1;
-
-	Handler mHandler = new Handler() {
-		public void dispatchMessage(android.os.Message msg) {
-			switch (msg.what) {
-			case STEPSHOW:
-				mHandler.removeMessages(STEPSHOW);
-				text_step.setText(StepDcretor.CURRENT_SETP + "");
-				mHandler.sendEmptyMessageDelayed(STEPSHOW, 150);
-				break;
-			}
-		};
-	};
+	private Handler mHandler;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mHandler = new Handler(this);
 		text_step = (TextView) findViewById(R.id.text_step);
 
 		Intent intent = new Intent(this, StepService.class);
@@ -40,5 +31,17 @@ public class MainActivity extends ActionBarActivity {
 	protected void onResume() {
 		super.onResume();
 		mHandler.sendEmptyMessage(STEPSHOW);
+	}
+
+	@Override
+	public boolean handleMessage(Message msg) {
+		switch (msg.what) {
+			case STEPSHOW:
+				mHandler.removeMessages(STEPSHOW);
+				text_step.setText(StepDcretor.CURRENT_SETP + "");
+				mHandler.sendEmptyMessageDelayed(STEPSHOW, 150);
+				break;
+		}
+		return false;
 	}
 }
